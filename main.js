35f8407eb603fc7ee66008d5957069e1bdd74112
anil wasisth <77065260+Anni1322@users.noreply.github.com>
2023-11-21 18:42:51 +0530

    first commit

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // import 
 require ('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const session = require("express-session");
  
 
@@ -9,12 +10,31 @@ const session = require("express-session");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+ 
+
 
 //database connectio 
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true});
-const db = mongoose.connection;
-db.on("error",(error)=>console.log(error));
-db.once("open", ()=> console.log("connected to the database!"));
+// MongoClient.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true,serverSelectionTimeoutMS: 30000,});
+// const db = MongoClient.connection;
+// db.on("error",(error)=>console.log(error));
+// db.once("open", ()=> console.log("connected to the database!"));
+
+const client = new MongoClient(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // Adjust the timeout as needed
+});
+
+client.connect()
+  .then(() => {
+    console.log("connected to the database!")
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
+
+
+
 
 // milddlewares
 app.use(express.urlencoded({ extended: false }));
